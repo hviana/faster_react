99b89397d955d8db18961112ed9d7d44f1b11b75
hviana <hv5088@gmail.com>
2024-10-30 12:55:34 -0300

    fix comments

--- app/backend/api/logout.ts
+++ app/backend/api/logout.ts
@@ -7,7 +7,7 @@ const logoutApiRoute = async (server: Server) => {
     req("json"),
     async (ctx: Context, next: NextFunc) => {
       console.log("call /user/logout api, content:");
-      console.log(ctx.body); //request json
+      console.log(ctx.body); //When using the req body parser middleare, the result is in ctx.body
       ctx.res.body = { logout: true }; //passes the request json as return
       await next(); //Calling await next(); is important to continue the flow of execution (or not, if you want to interrupt).
     },
